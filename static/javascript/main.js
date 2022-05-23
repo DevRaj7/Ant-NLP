33b5186035a596769f899e750482de2dbba408cb
chasmiccoder <chasmiccoder@gmail.com>
2022-05-23 12:37:23 +0530

    added: scrollable display box
    Displays the word problems as cards

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,3 +1,7 @@
+/*
+Controls the home page
+*/
+
 // The dataset drop area is the part of the page where the user can drop a json file to upload it (or click on the icon/upload button to upload the file)
 const datasetDropArea = document.querySelector('.dataset-drop-area');
 
@@ -7,6 +11,7 @@ const datasetUploadIcon   = datasetDropArea.querySelector('#upload-icon');  // c
 const datasetUploadInput  = datasetDropArea.querySelector('input');         // controls the file input button (which is hidden)
 
 var file;
+var data; // object that holds the dataset
 
 // the next 2 onclicks check if the user clicks on the upload button or the upload icon, and then trigger the input tag
 datasetUploadButton.onclick = () => {
@@ -18,8 +23,11 @@ datasetUploadIcon.onclick = () => {
 }
 
 // if the input tag is triggered, store the uploaded file in the file variable, and log the json data. (later, trigger a function that display the datapoints from the dataset in a list like fashion with cards (like a blog list))
-datasetUploadInput.addEventListener("change", () => {
-    file = this.files[0];
+datasetUploadInput.addEventListener("change", (event) => {
+    let dt = event.dataTransfer || (event.originalEvent && event.originalEvent.dataTransfer);
+    let files = event.target.files || (dt && dt.files);
+    file = files[0];
+
     processDataset();
 
     // the class 'active' is used to manipulate the drop area's css
@@ -28,6 +36,7 @@ datasetUploadInput.addEventListener("change", () => {
 
 // if the user drags the file over the drop area, prompt the user to release the file 
 datasetDropArea.addEventListener('dragover', (event) => {
+    event.stopPropagation();
     event.preventDefault();
     datasetDropArea.classList.add("active");
     dropAreaHeaderText.textContent = "Release to Upload File";
@@ -42,6 +51,7 @@ datasetDropArea.addEventListener('dragleave', () => {
 // if the user drops the file in the drop area
 datasetDropArea.addEventListener('drop', (event) => {
     // the default behaviour is to open the dropped image in a new tab
+    event.stopPropagation();
     event.preventDefault();
     console.log("File has been dropped in da");
 
@@ -64,7 +74,33 @@ function processDataset() {
         data = JSON.parse(data);
         console.log(data);
         console.log(typeof (data));
+        updateDatasetDisplay();
     }
 
     fileReader.readAsText(file);
+}
+
+// updates the display div with the word problems in the json file. Each card is a word problem that can be clicked on to access the word problem's annotation page
+function updateDatasetDisplay() {
+    let datasetDisplayDiv = document.querySelector('.dataset-display');
+    let size = Object.keys(data).length;
+
+    for(let i = 0; i < size; i++) {
+        // one datapoint
+        let dp = data[ Object.keys(data)[i] ];
+        let wordProblem = dp['word_problem'];
+        let answer = dp['answer'];
+        
+        let datapointCard = document.createElement('div');
+        datapointCard.classList.add('datapoint-card');
+        datapointCard.setAttribute('id', 'datapoint-card-' + (i+1).toString());
+        datapointCard.innerHTML = (i+1).toString() + ') ' + wordProblem;
+
+        // wrap the div around an anchor tag to make it clickable
+        // redirect the user to the annotation page of that word problem once clicked
+        let anchorTag = document.createElement('a');
+        anchorTag.href = 'problem-' + (i+1).toString();
+        anchorTag.appendChild(datapointCard);
+        datasetDisplayDiv.appendChild(anchorTag);
+    }
 }
