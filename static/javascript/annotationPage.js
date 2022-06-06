dcc0f1ea3cf9f23a86e781893716442848aeb08f
spikee <thegauravsaini007@gmail.com>
2022-06-06 11:49:42 +0530

    Prev,next buttons work

--- static/javascript/annotationPage.js
+++ static/javascript/annotationPage.js
@@ -196,3 +196,69 @@ const addNodeButton = document.querySelector('.div-add-node button');
 addNodeButton.onclick = () => {
     graphAddNode();
 }
+
+// Previous and Next buttons
+const prevButton = document.querySelector('.div-prev-problem').addEventListener('click', loadPrevProblem);
+const nextButton = document.querySelector('.div-next-problem').addEventListener('click', loadNextProblem);
+
+function loadPrevProblem() {
+    // Fetches the dataset from local storage (if it exists)
+    var xml = new XMLHttpRequest();
+    xml.open("POST", "/fetch-dataset", true);
+    xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xml.onload = function() {
+        var dataReply = JSON.parse(this.responseText);
+
+        // if the local database was not empty, then load it at the frontend
+        if (dataReply) {
+            data = JSON.parse(JSON.stringify(dataReply)); // deep copy of the dataset
+
+            // format this better using span/header/another p tag
+            if (problemNumber > 1) {
+                problemNumber = problemNumber - 1;
+                wordProblemDescription.innerHTML = problemNumber + ') ';
+                wordProblemDescription.innerHTML += data[problemNumber]['word_problem'];
+                wordProblemDescription.innerHTML += ' Answer: ' + data[problemNumber]['answer'];
+            } else {
+                problemNumber = problemNumber;
+                wordProblemDescription.innerHTML = problemNumber + ') ';
+                wordProblemDescription.innerHTML += data[problemNumber]['word_problem'];
+                wordProblemDescription.innerHTML += ' Answer: ' + data[problemNumber]['answer'];
+            }
+
+        }
+    };
+    xml.send(JSON.stringify({}));
+}
+
+function loadNextProblem() {
+    // Fetches the dataset from local storage (if it exists)
+    var xml = new XMLHttpRequest();
+    xml.open("POST", "/fetch-dataset", true);
+    xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xml.onload = function() {
+        var dataReply = JSON.parse(this.responseText);
+
+        // if the local database was not empty, then load it at the frontend
+        if (dataReply) {
+            data = JSON.parse(JSON.stringify(dataReply)); // deep copy of the dataset
+
+            // format this better using span/header/another p tag
+            if (problemNumber < 5) {
+                problemNumber = problemNumber + 1;
+                wordProblemDescription.innerHTML = problemNumber + ') ';
+                wordProblemDescription.innerHTML += data[problemNumber]['word_problem'];
+                wordProblemDescription.innerHTML += ' Answer: ' + data[problemNumber]['answer'];
+            } else {
+                problemNumber = problemNumber;
+                wordProblemDescription.innerHTML = problemNumber + ') ';
+                wordProblemDescription.innerHTML += data[problemNumber]['word_problem'];
+                wordProblemDescription.innerHTML += ' Answer: ' + data[problemNumber]['answer'];
+            }
+
+        }
+    };
+    xml.send(JSON.stringify({}));
+}
